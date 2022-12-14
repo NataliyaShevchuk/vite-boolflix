b971b00f4249e8356942706bfded2fb57104314e
NataliyaShevchuk <nata.shevchuk@icloud.com>
2022-12-14 23:41:53 +0100

    creazione file

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import {store} from "./store";
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      store,
+    }
+  },
+}
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss">
+@use "./style/general.scss" as *;
+
+</style>
